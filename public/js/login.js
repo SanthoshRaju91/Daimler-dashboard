b17bcd1579be8a20cdcc0d54499be5851adb2414
Santhosh Raju <santhosh.codeholic@gmail.com>
2016-04-19 12:12:19 +0530

    Login page change

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ var loginApp = angular.module('loginApp', []);
 
 loginApp.controller('FormController', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.login = function() {
+        console.log("Here");
         $http.post(document.URL+'api/login', angular.toJson({username: $scope.username, password: $scope.password}))    
             .then(function(response) {
                 console.log(response);
